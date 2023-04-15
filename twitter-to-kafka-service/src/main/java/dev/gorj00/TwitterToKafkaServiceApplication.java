9419a18eb703348347198047249f3f0108257c3d
Jan Gorol <os.jangorol@gmail.com>
2023-04-15 17:43:42 +0200

    twitter to kafka serve setup

---
+++ twitter-to-kafka-service/src/main/java/dev/gorj00/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,11 @@
+package dev.gorj00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+}
